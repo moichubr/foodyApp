9e5de2963a91864b6b3887f9f6c08a3a5bbbbd86
moichubr <brunmoira@gmail.com>
2023-06-03 10:32:17 -0300

    first commit front

---
+++ PI-Food-main/client/src/Views/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+const LandingPage = () => {
+    return(
+        <>
+        <h1>Foody</h1>
+        <h3>Tu App de recetas</h3>
+
+        <button>Ingresar</button>
+        </>
+    )
+}
+
+export default LandingPage;
